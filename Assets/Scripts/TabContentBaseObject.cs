5373b2a87d8def361202fc3987e364ef990ff38d
TakuKobayshi <>
2018-07-03 18:06:54 +0900

    Unity version up and construct tab view

---
+++ Assets/Scripts/TabContentBaseObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public abstract class TabContentBaseObject : MonoBehaviour
+{
+	public Button tabButton;
+	public GameObject content;
+}
---
+++ Assets/Scripts/TabController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabController : MonoBehaviour {
+
+	[SerializeField] private GameObject tabButtonField;
+	[SerializeField] private GameObject tabContentField;
+	[SerializeField] private List<GameObject> tabContents;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
