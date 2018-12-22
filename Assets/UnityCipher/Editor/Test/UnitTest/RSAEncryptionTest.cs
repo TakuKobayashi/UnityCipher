38f2e4ed05433c8aeb85808922e2cb74b6f147d9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-12-23 07:00:56 +0900

    Add Unit Test in RSAEncription

--- Assets/UnityCipher/Editor/Test/UnitTest/RSAEncryptionTest.cs
+++ Assets/UnityCipher/Editor/Test/UnitTest/RSAEncryptionTest.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
 using NUnit.Framework;
 
 namespace UnityCipher
@@ -8,60 +11,93 @@ namespace UnityCipher
     public class RSAEncryptionTest
     {
         [TestCase(1024)]
+        [TestCase(384)]
         public void GenrateKeyPairRegularTest(int keySize)
         {
+            bool isGeneratable = (keySize % 8 == 0) && 384 <= keySize && keySize <= 16384;
+            Assert.IsTrue(isGeneratable);
             KeyValuePair<string, string> publicPrivateKeypair1 = RSAEncryption.GenrateKeyPair(keySize);
             Assert.AreNotEqual(publicPrivateKeypair1.Key, publicPrivateKeypair1.Value);
-            UnityEngine.Debug.Log(publicPrivateKeypair1.Key.Length);
-            UnityEngine.Debug.Log(publicPrivateKeypair1.Value.Length);
             KeyValuePair<string, string> publicPrivateKeypair2 = RSAEncryption.GenrateKeyPair(keySize);
             Assert.AreNotEqual(publicPrivateKeypair1.Key, publicPrivateKeypair2.Key);
             Assert.AreNotEqual(publicPrivateKeypair1.Value, publicPrivateKeypair2.Value);
+            Assert.DoesNotThrow(() => RSAEncryption.GenrateKeyPair(keySize));
         }
 
-        [TestCase(1024)]
+        [TestCase(16385)]
+        [TestCase(376)]
+        [TestCase(1023)]
         public void GenrateKeyPairIrregularTest(int keySize)
         {
+            bool isGeneratable = (keySize % 8 == 0) && 384 <= keySize && keySize <= 16384;
+            Assert.IsFalse(isGeneratable);
+
+            CryptographicException exception = Assert.Throws<CryptographicException>(() => RSAEncryption.GenrateKeyPair(keySize));
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(exception.Message, "Key size not supported by algorithm.");
         }
 
-        [TestCase(1024)]
+        [TestCase(384)]
         public void EncryptStringRegularTest(int keySize)
         {
+            KeyValuePair<string, string> publicPrivateKeypair = RSAEncryption.GenrateKeyPair(keySize);
+            string planeString = Guid.NewGuid().ToString();
+            string encripted = RSAEncryption.Encrypt(planeString, publicPrivateKeypair.Key);
+            Assert.AreNotEqual(planeString, encripted);
         }
 
-        [TestCase(1024)]
-        public void EncryptStringIrregularTest(int keySize)
-        {
-        }
-
-        [TestCase(1024)]
+        [TestCase(392)]
         public void EncryptBinaryRegularTest(int keySize)
         {
+            KeyValuePair<string, string> publicPrivateKeypair = RSAEncryption.GenrateKeyPair(keySize);
+            byte[] planeBinary = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+            byte[] encripted = RSAEncryption.Encrypt(planeBinary, publicPrivateKeypair.Key);
+            Assert.AreNotEqual(planeBinary, encripted);
         }
 
-        [TestCase(1024)]
-        public void EncryptBinaryIrregularTest(int keySize)
-        {
-        }
-
-        [TestCase(1024)]
+        [TestCase(400)]
         public void DecryptStringRegularTest(int keySize)
         {
+            KeyValuePair<string, string> publicPrivateKeypair = RSAEncryption.GenrateKeyPair(keySize);
+            string planeString = Guid.NewGuid().ToString();
+            string encripted = RSAEncryption.Encrypt(planeString, publicPrivateKeypair.Key);
+            string decripted = RSAEncryption.Decrypt(encripted, publicPrivateKeypair.Value);
+            Assert.AreEqual(planeString, decripted);
         }
 
-        [TestCase(1024)]
+        [TestCase(1016)]
         public void DecryptStringIrregularTest(int keySize)
         {
+            KeyValuePair<string, string> publicPrivateKeypair1 = RSAEncryption.GenrateKeyPair(keySize);
+            string planeString = Guid.NewGuid().ToString();
+            string encripted = RSAEncryption.Encrypt(planeString, publicPrivateKeypair1.Key);
+            KeyValuePair<string, string> publicPrivateKeypair2 = RSAEncryption.GenrateKeyPair(keySize);
+
+            CryptographicException exception = Assert.Throws<CryptographicException>(() => RSAEncryption.Decrypt(encripted, publicPrivateKeypair2.Value));
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(exception.Message, "PKCS1 decoding error.");
         }
 
-        [TestCase(1024)]
+        [TestCase(408)]
         public void DecryptBinaryRegularTest(int keySize)
         {
+            KeyValuePair<string, string> publicPrivateKeypair = RSAEncryption.GenrateKeyPair(keySize);
+            byte[] planeBinary = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+            byte[] encripted = RSAEncryption.Encrypt(planeBinary, publicPrivateKeypair.Key);
+            byte[] decripted = RSAEncryption.Decrypt(encripted, publicPrivateKeypair.Value);
+            Assert.AreEqual(planeBinary, decripted);
         }
 
-        [TestCase(1024)]
+        [TestCase(1032)]
         public void DecryptBinaryIrregularTest(int keySize)
         {
+            KeyValuePair<string, string> publicPrivateKeypair1 = RSAEncryption.GenrateKeyPair(keySize);
+            byte[] planeBinary = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+            byte[] encripted = RSAEncryption.Encrypt(planeBinary, publicPrivateKeypair1.Key);
+            KeyValuePair<string, string> publicPrivateKeypair2 = RSAEncryption.GenrateKeyPair(keySize);
+            CryptographicException exception = Assert.Throws<CryptographicException>(() => RSAEncryption.Decrypt(encripted, publicPrivateKeypair2.Value));
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(exception.Message, "PKCS1 decoding error.");
         }
     }
 }
