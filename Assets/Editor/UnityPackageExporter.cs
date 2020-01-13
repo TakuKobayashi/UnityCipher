7a7e353f2e38a26e69c04048061ee58a66c31e21
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-01-13 17:28:08 +0900

    ExportUnityPackage

---
+++ Assets/Editor/UnityPackageExporter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Compilation;
+using System.Diagnostics;
+using System.Reflection;
+
+public class UnityPackageExporter
+{
+    public static void ExportUnityPackageRoutine(
+        string exportDllPath,
+        string dllBuildIncludeRootPath,
+        string buildIncludeRootPath,
+        string exportFilePath
+    )
+    {
+        List<string> buildPathes = FindFilePathes(buildIncludeRootPath);
+        ExportUnityPackage(buildPathes.ToArray(), exportFilePath);
+        string[] pathCells = exportFilePath.Split("/".ToCharArray());
+        pathCells[pathCells.Length - 1] = "";
+        // 保存先フォルダを開く
+        Process.Start(string.Join("/", pathCells));
+    }
+
+    public static void ExportUnityPackage(string[] buildIncludeRootPathes, string exportFilePath)
+    {
+        AssetDatabase.ExportPackage(buildIncludeRootPathes, exportFilePath, ExportPackageOptions.Recurse);
+    }
+
+    public static List<string> FindFilePathes(string filterName, string extFileName = "")
+    {
+        List<string> seachedFilePathes = new List<string>();
+        string[] pathes = AssetDatabase.GetAllAssetPaths();
+        for (int i = 0; i < pathes.Length; ++i)
+        {
+            string path = pathes[i];
+            Match match = Regex.Match(path.ToLower(), @"" + filterName.ToLower() + ".+" + extFileName);
+            if (match.Success)
+            {
+                seachedFilePathes.Add(path);
+            }
+        }
+        return seachedFilePathes;
+    }
+}
